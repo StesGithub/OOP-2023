3e230cc9d21186159504e15f6f13741a38aa1d47
StesGithub <123895888+StesGithub@users.noreply.github.com>
2023-04-23 15:03:10 +0100

    play and pause stuff

--- java/src/ie/tudublin/stephenCode.java
+++ java/src/ie/tudublin/stephenCode.java
@@ -4,12 +4,14 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
-import processing.core.PApplet;;
+import processing.core.PApplet;
 
 public class stephenCode extends PApplet {
 
     Minim minim;
     AudioPlayer player;
+    AudioPlayer scale;
+    AudioPlayer another;
 
     public void settings() {
         size(512, 200, P3D);
@@ -19,43 +21,76 @@ public class stephenCode extends PApplet {
 
         minim = new Minim(this);
 
-        player = minim.loadFile("heroplanet.mp3");
+        player = minim.loadFile("scale.wav");
+        scale = minim.loadFile("scale.wav");
+        another = minim.loadFile("tomp3.cc");
+        // player.loop();
     }
 
     public void draw() {
-        background(0);
-        stroke(255);
-
         /*
+         * background(0);
+         * stroke(255);
+         * 
+         * 
          * Drawing the waveforms
          * the values returned by left.get() and right.get() will be between -1 and 1
          * so we need them scaled up to see the wave form
          * note that if the file is mono, left.get() and right.get() will return the
          * same value
+         * 
+         * for (int i = 0; i < player.bufferSize() - 1; i++) {
+         * float x1 = map(i, 0, player.bufferSize(), 0, width);
+         * float x2 = map(i + 1, 0, player.bufferSize(), 0, width);
+         * line(x1, 50 + player.left.get(i) * 50, x2, 50 + player.left.get(i + 1) * 50);
+         * line(x1, 150 + player.right.get(i) * 50, x2, 150 + player.right.get(i + 1) *
+         * 50);
+         * }
+         * 
+         * float posx = map(player.position(), 0, player.length(), 0, width);
+         * stroke(0, 200, 0);
+         * line(posx, 0, posx, height);
+         * 
+         * if (player.isPlaying()) {
+         * text("press any key to pause playback", 10, 20);
+         * } else {
+         * text("Press any key to start playback", 10, 20);
+         * 
+         * }
+         * 
          */
-        for (int i = 0; i < player.bufferSize() - 1; i++) {
-            float x1 = map(i, 0, player.bufferSize(), 0, width);
-            float x2 = map(i + 1, 0, player.bufferSize(), 0, width);
-            line(x1, 50 + player.left.get(i) * 50, x2, 50 + player.left.get(i + 1) * 50);
-            line(x1, 150 + player.right.get(i) * 50, x2, 150 + player.right.get(i + 1) * 50);
+
+        background(255);
+        fill(255, 0, 0);
+        rect(25, 25, 50, 50);// 25,25 -- coords 50,50 -- 50 pixels wide 50 pixels tall
+
+        fill(0, 255, 00);
+        rect(125, 25, 50, 50);
+    }
+
+    public void mousePressed() {
+        if (mouseX > 25 && mouseX < 75 && mouseY > 25 && mouseY < 75) {
+            scale.rewind();
+            scale.play();
         }
 
-        float posx = map(player.position(), 0, player.length(), 0, width);
-        stroke(0, 200, 0);
-        line(posx, 0, posx, height);
-
-        if (player.isPlaying()) {
-            text("press any key to pause playback", 10, 20);
-        } else {
-            text("Press any key to start playback", 10, 20);
-
+        if (mouseX > 125 && mouseX < 175 && mouseY > 25 && mouseY < 75) {
+            scale.pause();
         }
 
     }
 
-    public void keyPressed() {
+   public void keyPressed() {
 
-        if (player.isPlaying()) {
+
+    if (key == 'e'){
+        scale.pause();
+    }
+    if(key == 'q'){
+        scale.play();
+    }
+       /*
+       if (player.isPlaying()) {
             player.pause();
         }
 
@@ -67,5 +102,6 @@ public class stephenCode extends PApplet {
             player.play();
         }
     }
-
+        */ 
+    }
 }
